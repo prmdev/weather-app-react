6e0723d40bc8637cd0fa9aad80e907b4ba19aac6
prmdev <93345906+prmdev@users.noreply.github.com>
2018-02-06 14:16:24 -0600

    misc

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class GoogleMap extends Component {
-    componentDidMount(){
-        new google.maps.Map(this.refs.map,{
-            zoom: 12,
-            center: {
-                lat: this.props.lat,
-                lng: this.props.lon
-            }
-        });
-    }
+// class GoogleMap extends Component {
+//     componentDidMount(){
+//         new google.maps.Map(this.refs.map,{
+//             zoom: 12,
+//             center: {
+//                 lat: this.props.lat,
+//                 lng: this.props.lon
+//             }
+//         });
+//     }
 
-    render(){
-        return <div ref="map"/>
-    }
-}
+//     render(){
+//         return <div ref="map"/>
+//     }
+// }
 
-export default GoogleMap; 
+// export default GoogleMap; 
